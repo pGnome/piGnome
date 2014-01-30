31dad93fd9998734db34cb620eed2bfca4d61952
KrisNi <krisnii.com@gmail.com>
2014-01-29 23:25:34 -0500

    add pump trigger program

---
+++ pump_trigger.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+db = sqlite3.connect("myDBfile.sqlite3")
+
+
+def pump_sig():
+	cur.execute('''SELECT MoistureLevel 
+		FROM levelSet
+		ORDER BY SettingTime DESC
+		LIMIT 1
+		''')
+	setting = cur.fetchone()
+
+	cur.execute('''SELECT MoistureLevel
+		FROM pGnome
+		GROUP BY GnomeName
+		ORDER BY CollectedTime DESC
+		''')
+	readings = cur.fetchall()
+
+	for reading in readings:
+		print reading
+		if setting[0] <= reading[0]:
+			print 0
+		else:
+			print 1
+		
+
+cur = db.cursor()
+
+db.commit()
+pump_sig()
+db.close()
+	
