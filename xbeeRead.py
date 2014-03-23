49680fd5a0682416bdd10bb2ffcadfc9a4d4a6ad
Jonathan Kolnik <kolnik.com@gmail.com>
2014-03-23 19:10:19 +0000

    read correct format

--- xbeeRead.py
+++ xbeeRead.py
@@ -1,6 +1,10 @@
 import serial
 serialport = serial.Serial("/dev/ttyAMA0", 9600, timeout=5.5)
 while True:
-	response = serialport.read(size=1)
-	print response
+	response = serialport.read(size=26)
+	if response.__len__() > 0:
+		for c in response:
+			print ord(c)
+	else:
+		print "length is 0"
 
