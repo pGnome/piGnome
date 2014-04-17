13b2a58549f9b84789e22c2a9bca6beba5ca54fd
KrisNi <krisnii.com@gmail.com>
2014-04-17 16:28:56 -0400

    try multi-tread

--- pump.py
+++ pump.py
@@ -8,24 +8,24 @@ def pump_sig(identifier,gpio_pins):
 	myDatabase = sqlite3.connect("myDBfile.sqlite3", check_same_thread=False)
 	cur = myDatabase.cursor()
 	
-		cur.execute('''SELECT gnomeRecords.GnomeZone
-			FROM (SELECT MoistureLevel, GnomeZone, MAX(RecordId) as gnomeId
-				  FROM pGnome
-				  GROUP BY GnomeZone) as gnomeRecords, levelSet
-			WHERE gnomeRecords.GnomeZone = levelSet.GnomeZone AND
-				  gnomeRecords.MoistureLevel < levelSet.MoistureLevel
-			''')
-		settings = cur.fetchall()
-		
+	cur.execute('''SELECT gnomeRecords.GnomeZone
+		FROM (SELECT MoistureLevel, GnomeZone, MAX(RecordId) as gnomeId
+			  FROM pGnome
+			  GROUP BY GnomeZone) as gnomeRecords, levelSet
+		WHERE gnomeRecords.GnomeZone = levelSet.GnomeZone AND
+			  gnomeRecords.MoistureLevel < levelSet.MoistureLevel
+		''')
+	settings = cur.fetchall()
+	
 
-		for setting in settings:
-			GPIO.output(gpio_pins[0], GPIO.HIGH)
-			if setting == 1:
-				GPIO.output(gpio_pins[1], GPIO.HIGH)
-			elif setting == 2:
-				GPIO.output(gpio_pins[2], GPIO.HIGH)
-			elif setting == 3:
-				GPIO.output(gpio_pins[3], GPIO.HIGH)
+	for setting in settings:
+		GPIO.output(gpio_pins[0], GPIO.HIGH)
+		if setting == 1:
+			GPIO.output(gpio_pins[1], GPIO.HIGH)
+		elif setting == 2:
+			GPIO.output(gpio_pins[2], GPIO.HIGH)
+		elif setting == 3:
+			GPIO.output(gpio_pins[3], GPIO.HIGH)
 	try:	
 		myDatabase.commit()
 	except Exception:
