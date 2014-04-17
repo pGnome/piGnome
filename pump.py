b24186aa67986bc228d57fda5df6450ab5cf7fee
KrisNi <krisnii.com@gmail.com>
2014-04-17 16:43:24 -0400

    try multi-tread

--- pump.py
+++ pump.py
@@ -7,25 +7,32 @@ def pump_sig(identifier,gpio_pins):
 	#connect to the local database#
 	myDatabase = sqlite3.connect("myDBfile.sqlite3", check_same_thread=False)
 	cur = myDatabase.cursor()
-	
-	cur.execute('''SELECT gnomeRecords.GnomeZone
-		FROM (SELECT MoistureLevel, GnomeZone, MAX(RecordId) as gnomeId
-			  FROM pGnome
-			  GROUP BY GnomeZone) as gnomeRecords, levelSet
-		WHERE gnomeRecords.GnomeZone = levelSet.GnomeZone AND
-			  gnomeRecords.MoistureLevel < levelSet.MoistureLevel
-		''')
-	settings = cur.fetchall()
-	
 
-	for setting in settings:
-		GPIO.output(gpio_pins[0], GPIO.HIGH)
-		if setting == 1:
-			GPIO.output(gpio_pins[1], GPIO.HIGH)
-		elif setting == 2:
-			GPIO.output(gpio_pins[2], GPIO.HIGH)
-		elif setting == 3:
-			GPIO.output(gpio_pins[3], GPIO.HIGH)
+	while True:
+		try:
+			cur.execute('''SELECT gnomeRecords.GnomeZone
+				FROM (SELECT MoistureLevel, GnomeZone, MAX(RecordId) as gnomeId
+					  FROM pGnome
+					  GROUP BY GnomeZone) as gnomeRecords, levelSet
+				WHERE gnomeRecords.GnomeZone = levelSet.GnomeZone AND
+					  gnomeRecords.MoistureLevel < levelSet.MoistureLevel
+				''')
+			settings = cur.fetchall()
+			
+
+			for setting in settings:
+				GPIO.output(gpio_pins[0], GPIO.HIGH)
+				if setting == 1:
+					GPIO.output(gpio_pins[1], GPIO.HIGH)
+				elif setting == 2:
+					GPIO.output(gpio_pins[2], GPIO.HIGH)
+				elif setting == 3:
+					GPIO.output(gpio_pins[3], GPIO.HIGH)
+
+			break;
+		except Exception:
+			unlock_db("myDBfile.sqlite3")
+			
 	try:	
 		myDatabase.commit()
 	except Exception:
